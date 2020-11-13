1b219b1a44ee9e71fa1d9d09bd88921d3e895cfa
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-11-13 18:35:09 -0300

    Creating colors enums

---
+++ src/enums/colors.js
@@ -0,0 +1,12 @@
+export default {
+  black: '#212121',
+  dark: '#1E2328',
+  darkGray: '#2A2E34',
+  gray: '#3B3F46',
+  orange: '#F5A302',
+  lightOrange: '#F5B301',
+  yellow: '#FED053',
+  grayLight: '#E0E0E0',
+  whiteDark: '#f5f5f5',
+  white: '#FFFFFF',
+}
