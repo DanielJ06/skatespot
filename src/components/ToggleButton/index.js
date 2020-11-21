11119b7fa0d291372a8d3595c73bb4bd559a50cd
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-11-21 13:04:23 -0300

    Toggle button component

---
+++ src/components/ToggleButton/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import colors from '../../enums/colors';
+
+import { Container, Title, Gradient } from './styles';
+
+const ToggleButton = ({ data }) => {
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const handleSelectItem = (id) => {
+    const alreadySelected = selectedItems.findIndex((item) => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter((item) => item !== id);
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  };
+
+  return (
+    <Container onPress={() => handleSelectItem(data.id)} >
+      <Gradient
+        isSelected={selectedItems.includes(data.id) ? true : false}
+        colors={[colors.orange, colors.lightOrange]}
+      >
+        <Title>{data.name}</Title>
+      </Gradient>
+    </Container>
+  );
+}
+
+export default ToggleButton;
---
+++ src/components/ToggleButton/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+
+import { LinearGradient } from 'expo-linear-gradient';
+import colors from '../../enums/colors';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled(RectButton)`
+  width: 155px;
+  height: 83px;
+  border-radius: 15px;
+  background-color: ${colors.darkOrange};
+`;
+
+export const Title = styled.Text`
+  color: ${colors.white};
+  font-weight: bold;
+`;
+
+export const Gradient = styled(LinearGradient)`
+  flex: 1;
+  opacity: ${props => props.isSelected ? 1 : 0.5};
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  border-radius: 15px;
+`;
